5de6a5d4ad4670709ce638fd4a54e842b4f8e686
yosif11 <49305870+yosif11@users.noreply.github.com>
2020-08-09 18:10:10 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,161 @@
+const express = require("express");
+const app = express();
+
+app.listen(() => console.log("start btrolie"));
+
+app.use('/ping', (req, res) => {
+  res.send(new Date());
+});
+
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const cmd = require("node-cmd");
+const ms = require("ms");
+const fs = require('fs');
+const ytdl = require("ytdl-core");
+const canvas = require("canvas");
+const convert = require("hh-mm-ss")
+const fetchVideoInfo = require("youtube-info");
+const simpleytapi = require('simple-youtube-api')
+const util = require("util")
+const gif = require("gif-search");
+const jimp = require("jimp");
+const guild = require('guild');
+const hastebins = require('hastebin-gen');
+const getYoutubeID = require('get-youtube-id');
+const pretty = require("pretty-ms");
+const moment = require('moment');
+const request = require('request');
+const dateFormat = require('dateformat');
+
+//لا تلعب اي شي في الكود
+
+
+
+const prefix = "!"
+const developers = "496996678088392725"
+
+////////
+
+const category = "674270821921062913";
+let mtickets   = true;
+let tchannels  = [];
+let current    = 0;
+
+
+
+client.on("message", message => {
+  
+  if(message.author.bot) return;
+  
+  if(!message.content.startsWith(prefix)) return;
+  
+  if(message.content === (prefix + 'help')) {
+ 
+     message.channel.send(`** قائمه اوامر التكت 
+			                 
+\`\`${prefix}new\`\` | لفتح تكت
+
+\`\`${prefix}close\`\` | لغلق التكت
+
+\`\`${prefix}mtickets enable/disable\`\` | لتعطيل وعدم تعطيل التكت
+
+\`\`${prefix}cleartickets\`\` | لمسح جميع التكتات **`) 
+     
+     } 
+})
+
+
+
+
+client.on('message',async message => {
+    if(message.author.bot || message.channel.type === 'dm') return;
+    let args = message.content.split(" ");
+    let author = message.author.id;
+        if(message.content.startsWith(prefix + 'لاتلعب بذا اساس تشغيل')) {
+            let embed = new Discord.RichEmbed()
+            .setAuthor(message.author.username, message.author.avatarURL)
+            .setThumbnail(message.author.avatarURL)
+            .setColor("#36393e")
+			.addField(`تم التطوير من قبل بترولي`)
+            .addField(`تم التطوير من قبل بترولي`)
+            .addField(`تم التطوير من قبل بترولي`)
+			.addField(`تم التطوير من قبل بترولي`)
+            .addField(`تم التطوير من قبل بترولي`)
+            await message.channel.send(``);
+            await message.channel.send(embed);
+    } else if(args[0].toLowerCase() === `${prefix}new`) {
+        if(mtickets === false) return message.channel.send(`:tools: , **تم ايقاف هذه الخاصية من قبل احد ادارة السيرفر**`);
+        if(!message.guild.me.hasPermission("MANAGE_CHANNELS")) return message.channel.send(`:tools: , **البوت لا يملك صلاحيات لصنع الروم**`);
+		console.log(current);
+		let openReason = "";
+		current++;
+    	message.guild.createChannel(`ticket-${current}`, 'text').then(c => {
+		tchannels.push(c.id);
+		c.setParent(category);
+		message.channel.send(`**:tickets: تم عمل التكت.**`);
+		c.overwritePermissions(message.guild.id, {
+			READ_MESSAGES: false,
+			SEND_MESSAGES: false
+		});
+		c.overwritePermissions(message.author.id, {
+			READ_MESSAGES: true,
+			SEND_MESSAGES: true
+		});
+		
+		if(args[1]) openReason = `\nسبب فتح التكت , " **${args.slice(1).join(" ")}** "`;
+		let embed = new Discord.RichEmbed()
+		.setAuthor(message.author.username, message.author.avatarURL)
+		.setColor("#36393e")
+		.setDescription(`**انتظر قليلا الى حين رد الادارة عليك**${openReason}`);
+		c.send(`${message.author}`);
+		c.send(embed);
+	});
+    } else if(args[0].toLowerCase() === `${prefix}mtickets`) {
+        if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send(`:tools: , **أنت لست من ادارة السيرفر لتنفيذ هذا الأمر.**`);
+		if(args[1] && args[1].toLowerCase() === "enable") {
+			mtickets = true;
+			message.channel.send(`:white_check_mark: , **تم تفعيل التكتات , الاَن يمكن لأعضاء السيرفر استخدام امر انشاء التكت**`);
+		} else if(args[1] && args[1].toLowerCase() === "disable") {
+			mtickets = false;
+			message.channel.send(`:white_check_mark: , **تم اغلاق نظام التكتات , الاَن لا يمكن لأي عضو استخدام هذا الأمر**`);
+		} else if(!args[1]) {
+			if(mtickets === true) {
+			mtickets = false;
+			message.channel.send(`:white_check_mark: , **تم اغلاق نظام التكتات , الاَن لا يمكن لأي عضو استخدام هذا الأمر**`);
+			} else if(mtickets === false) {
+			mtickets = true;
+			message.channel.send(`:white_check_mark: , **تم تفعيل التكتات , الاَن يمكن لأعضاء السيرفر استخدام امر انشاء التكت**`);
+			}
+		}
+    } else if(args[0].toLowerCase() === `${prefix}close`) {
+		if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send(`:tools:, **أنت لست من ادارة السيرفر لتنفيذ هذا الأمر.**`);
+		if(!message.channel.name.startsWith('ticket-') && !tchannels.includes(message.channel.id)) return message.channel.send(`:tools:, **هذا الروم ليس من رومات التكت.**`);
+		
+		message.channel.send(`:white_check_mark:, **سيتم اغلاق الروم في 3 ثواني من الاَن.**`);
+		tchannels.splice( tchannels.indexOf(message.channel.id), 1 );
+		setTimeout(() => message.channel.delete(), 3000);
+	} else if(args[0].toLowerCase() === `${prefix}restart`) {
+		if(!devs.includes(message.author.id)) return message.channel.send(`:tools:, **أنت لست من ادارة السيرفر لأستخدام هذا الأمر.**`);
+		message.channel.send(`:white_check_mark:, **جارى اعادة تشغيل البوت.**`);
+		client.destroy();
+	} else if(args[0].toLowerCase() === `${prefix}deletetickets`) {
+		let iq = 0;
+		for(let q = 0; q < tchannels.length; q++) {
+			let c = message.guild.channels.get(tchannels[q]);
+			if(c) {
+				c.delete();
+				tchannels.splice( tchannels[q], 1 );
+				iq++;
+			}
+			if(q === tchannels.length - 1 || q === tchannels.lengh + 1) {
+				message.channel.send(`:white_check_mark:, **تم مسح \`${iq}\` من التكتات.**`);
+			}
+		}
+	}
+});
+
+
+
+client.login("NzM5NTUxOTczODQxNTY3ODE0.XycHcg.1NZMChRG2amrINxegf1s7f0Q6K4");
